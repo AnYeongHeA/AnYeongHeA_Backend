7b4d9db7def49b8dce17de5cd250adba7b9a60c8
SunSet0418 <coffee@soylatte.kr>
2018-06-18 17:19:00 +0900

    add database setting file

---
+++ setting.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE 2018MobileContent;
+
+USE 2018MobileContent;
+
+CREATE TABLE user(
+  username VARCHAR(100) NOT NULL ,
+  schoolName VARCHAR(150) NOT NULL,
+  schoolNumber VARCHAR(20) NOT NULL ,
+  birthday VARCHAR(20) NOT NULL ,
+  password VARCHAR(200) NOT NULL ,
+  usertoken VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE photobook(
+  name VARCHAR(200) NOT NULL ,
+  photo VARCHAR(200) NOT NULL ,
+  count INT(11) NOT NULL ,
+  summary VARCHAR(250) NOT NULL ,
+  since VARCHAR(20) NOT NULL ,
+  booktoken VARCHAR(250) NOT NULL,
+  usertoken VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE photo(
+  booktoken VARCHAR(250) NOT NULL ,
+  summary VARCHAR(250) NOT NULL ,
+  photo VARCHAR(250) NOT NULL
+);
